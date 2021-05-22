ede20cecc9e46dc59c0824e82995a2be3dba2bdf
Arshad <arshadpinjari666@gmail.com>
2021-05-22 16:12:53 +0530

    Add project files.

---
+++ RegexPatternValidationDemo/Patterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternValidationDemo
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}";
+        public bool validationPincode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
---
+++ RegexPatternValidationDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexPatternValidationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns pattern = new Patterns();
+            Console.WriteLine(pattern.validationPincode("400088"));
+        }
+    }
+}
